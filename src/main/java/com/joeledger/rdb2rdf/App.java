a587aac0c265f0e110fdc330be524f336f8ee356
joewledger <joewledger@gmail.com>
2016-11-21 15:11:58 -0500

    Removed SQL checks in getAttributeNames and getTableNames for code clarity
    Signed-off-by: joewledger <joewledger@gmail.com>

--- src/main/java/com/joeledger/rdb2rdf/App.java
+++ src/main/java/com/joeledger/rdb2rdf/App.java
@@ -15,49 +15,49 @@ public class App
         Set<MutableTriple<String, String, String>> rdfTriples = new HashSet<>();
         rdfTriples.add(new MutableTriple<>("a", "b", "c"));
 
-        Set<String> tableNames = getTableNames();
-        System.out.println(tableNames);
+        try {
 
-        tableNames.stream().forEach(s -> System.out.println(s + ": " + getAttributeNames(s)));
+            Set<String> tableNames = getTableNames();
+            System.out.println(tableNames);
+
+            for(String t : tableNames){
+                System.out.println(getAttributeNames(t));
+            }
+
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    public static Set<String> getAttributeNames(String tableName){
+    public static Set<String> getAttributeNames(String tableName) throws SQLException {
         Set<String> attributeNames = new HashSet<>();
 
-        try {
-            Connection connection = connect();
+        Connection connection = connect();
 
-            String query = String.format("SELECT * FROM %s", tableName);
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet rs = statement.executeQuery();
-            ResultSetMetaData rsmd = rs.getMetaData();
+        String query = String.format("SELECT * FROM %s", tableName);
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
 
-            int columnCount = rsmd.getColumnCount();
-            for (int i = 1; i <= columnCount; i++ ) {
-                attributeNames.add(rsmd.getColumnName(i));
-            }
-            return attributeNames;
-        } catch (Exception e) {
-            return attributeNames;
+        int columnCount = rsmd.getColumnCount();
+        for (int i = 1; i <= columnCount; i++ ) {
+            attributeNames.add(rsmd.getColumnName(i));
         }
+        return attributeNames;
     }
 
-    public static Set<String> getTableNames(){
+    public static Set<String> getTableNames() throws SQLException {
         Set<String> tableNames = new HashSet<>();
-        try {
-            Connection connection = connect();
-            DatabaseMetaData md  = connection.getMetaData();
+        Connection connection = connect();
+        DatabaseMetaData md  = connection.getMetaData();
 
-            ResultSet rs = md.getTables(null, null, "%", new String[] {"TABLE"});
-            while (rs.next()) {
-                tableNames.add(rs.getString("TABLE_NAME"));
-            }
-
-            return tableNames.stream().filter(s -> !s.startsWith("sqlite")).collect(Collectors.toSet());
-        } catch (Exception e) {
-            return null;
+        ResultSet rs = md.getTables(null, null, "%", new String[] {"TABLE"});
+        while (rs.next()) {
+            tableNames.add(rs.getString("TABLE_NAME"));
         }
+
+        return tableNames.stream().filter(s -> !s.startsWith("sqlite")).collect(Collectors.toSet());
     }
 
     public static Connection connect() {
