81c3fb3186496a5390dd817f5d51f2a28d7b7b0a
joewledger <joewledger@gmail.com>
2016-11-21 14:21:13 -0500

    Added code to get listing of table names
    Signed-off-by: joewledger <joewledger@gmail.com>

--- src/main/java/com/joeledger/rdb2rdf/App.java
+++ src/main/java/com/joeledger/rdb2rdf/App.java
@@ -5,30 +5,43 @@ import java.util.HashSet;
 import org.apache.commons.lang3.tuple.MutableTriple;
 
 import java.sql.*;
+import java.util.stream.Collectors;
 
 public class App 
 {
+
     public static void main( String[] args ) {
 
         Set<MutableTriple<String, String, String>> rdfTriples = new HashSet<>();
         rdfTriples.add(new MutableTriple<>("a", "b", "c"));
 
-        Connection c = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:chinook.db");
+        getTableNames().stream().forEach(System.out::println);
+    }
 
-            PreparedStatement statement = c.prepareStatement("SELECT Name from genres");
-            ResultSet result = statement.executeQuery();
-            while(result.next()) {
-                System.out.println(result.getString("Name"));
+    public static Set<String> getTableNames(){
+        Set<String> tableNames = new HashSet<>();
+        try {
+            Connection connection = connect();
+            DatabaseMetaData md  = connection.getMetaData();
+
+            ResultSet rs = md.getTables(null, null, "%", new String[] {"TABLE"});
+            while (rs.next()) {
+                tableNames.add(rs.getString("TABLE_NAME"));
             }
 
+            return tableNames.stream().filter(s -> !s.startsWith("sqlite")).collect(Collectors.toSet());
         } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+            return null;
         }
-        System.out.println("Opened database successfully");
-
     }
+
+    public static Connection connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection("jdbc:sqlite:chinook.db");
+        } catch (ClassNotFoundException | SQLException e) {
+            return null;
+        }
+    }
+
 }
