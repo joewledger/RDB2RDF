aea839e9807cfd1afdbae6e16944b44da511e585
joewledger <joewledger@gmail.com>
2016-11-22 11:03:23 -0500

    Set up structure for building a set of RDF tuples
    Signed-off-by: joewledger <joewledger@gmail.com>

--- src/main/java/com/joeledger/rdb2rdf/App.java
+++ src/main/java/com/joeledger/rdb2rdf/App.java
@@ -1,55 +1,32 @@
 package com.joeledger.rdb2rdf;
 
-import java.util.Set;
-import java.util.HashSet;
-import org.apache.commons.lang3.tuple.MutableTriple;
-
+import java.util.*;
+import java.util.stream.*;
 import java.sql.*;
-import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.*;
 
 public class App 
 {
 
     public static void main( String[] args ) {
 
-        Set<MutableTriple<String, String, String>> rdfTriples = new HashSet<>();
-        rdfTriples.add(new MutableTriple<>("a", "b", "c"));
+        String dbName = "chinook.db";
+        Set<Triple<String, String, String>> rdfTriples = new HashSet<>();
 
         try {
-
-            Set<String> tableNames = getTableNames();
-            System.out.println(tableNames);
-
-            for(String t : tableNames){
-                System.out.println(getAttributeNames(t));
+            for(String tableName : getTableNames(dbName)) {
+                Table table = new Table(dbName, tableName);
+                rdfTriples.addAll(table.getTripleSet());
+                System.out.println(rdfTriples);
             }
-
-        } catch (java.sql.SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Unable to access database");
         }
     }
 
-
-    public static Set<String> getAttributeNames(String tableName) throws SQLException {
-        Set<String> attributeNames = new HashSet<>();
-
-        Connection connection = connect();
-
-        String query = String.format("SELECT * FROM %s", tableName);
-        PreparedStatement statement = connection.prepareStatement(query);
-        ResultSet rs = statement.executeQuery();
-        ResultSetMetaData rsmd = rs.getMetaData();
-
-        int columnCount = rsmd.getColumnCount();
-        for (int i = 1; i <= columnCount; i++ ) {
-            attributeNames.add(rsmd.getColumnName(i));
-        }
-        return attributeNames;
-    }
-
-    public static Set<String> getTableNames() throws SQLException {
+    public static Set<String> getTableNames(String dbName) throws SQLException {
         Set<String> tableNames = new HashSet<>();
-        Connection connection = connect();
+        Connection connection = DBUtils.connect(dbName);
         DatabaseMetaData md  = connection.getMetaData();
 
         ResultSet rs = md.getTables(null, null, "%", new String[] {"TABLE"});
@@ -59,14 +36,4 @@ public class App
 
         return tableNames.stream().filter(s -> !s.startsWith("sqlite")).collect(Collectors.toSet());
     }
-
-    public static Connection connect() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:chinook.db");
-        } catch (ClassNotFoundException | SQLException e) {
-            return null;
-        }
-    }
-
 }
--- src/main/java/com/joeledger/rdb2rdf/Table.java
+++ src/main/java/com/joeledger/rdb2rdf/Table.java
@@ -1,20 +1,24 @@
 package com.joeledger.rdb2rdf;
 
+import java.sql.*;
 import java.util.*;
 import org.apache.commons.lang3.tuple.*;
 
 public class Table {
 
+    private String dbName;
     private String tableName;
     private Set<String> primaryKeys;
     private Map<String, String> references;
     private Set<String> columnSet;
 
-    public Table(String tableName) {
+    public Table(String dbName, String tableName) {
+        this.dbName = dbName;
         this.tableName = tableName;
         this.primaryKeys = new HashSet<>();
         this.references = new HashMap<>();
         this.columnSet = new HashSet<>();
+        this.fillWithMetadata();
     }
 
     public Set<Triple<String, String, String>> getTripleSet() {
@@ -25,6 +29,23 @@ public class Table {
     private boolean isManyToMany() {
         return false;
     }
-    
 
+    private void fillWithMetadata() {
+
+
+    }
+
+    public void getColumnNames(String tableName) throws SQLException {
+        Connection connection = DBUtils.connect(this.dbName);
+
+        String query = String.format("SELECT * FROM %s", tableName);
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        int columnCount = rsmd.getColumnCount();
+        for (int i = 1; i <= columnCount; i++) {
+            this.columnSet.add(rsmd.getColumnName(i));
+        }
+    }
 }
