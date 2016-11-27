17422990f4ec1888a861cdcd77de9e6f6d1ee122
joewledger <joewledger@gmail.com>
2016-11-27 17:07:38 -0500

    Finished RDF conversion for non-M2M tables
    Signed-off-by: joewledger <joewledger@gmail.com>

--- src/main/java/com/joeledger/rdb2rdf/Table.java
+++ src/main/java/com/joeledger/rdb2rdf/Table.java
@@ -41,9 +41,36 @@ public class Table {
     }
 
     private Set<Triple<String, String, String>> getStandardTripleSet() throws SQLException {
-        return null;
+        Set<Triple<String, String, String>> rdfTriples = new HashSet<>();
+        ResultSet rdbTuples = DBUtils.getFullTableResultSet(this.dbName, this.tableName);
 
+        Set<String> referenceColumns = this.foreignKeyReferences.keySet();
+        Set<String> valueColumns = this.columnSet.stream()
+                .filter(s -> !referenceColumns.contains(s) && !this.primaryKeys.contains(s))
+                .collect(Collectors.toSet());
 
+        String pKey = this.primaryKeys.stream().collect(Collectors.toList()).get(0);
+
+        Map<String, String> valueColPredicates = getValueColPredicates(valueColumns);
+        Map<String, String> referenceColPredicates = getReferenceColPredicates(referenceColumns);
+
+        while(rdbTuples.next()){
+            String subject = getSubject(rdbTuples, pKey);
+
+            rdfTriples.add(new ImmutableTriple<>(subject, "rdf:type", String.format("<%s>", this.tableName)));
+
+            for(String valueColumn : valueColumns) {
+                String predicate = valueColPredicates.get(valueColumn);
+                rdfTriples.add(new ImmutableTriple<>(subject, predicate, rdbTuples.getString(valueColumn)));
+            }
+
+            for(String refColumn : referenceColumns) {
+                String predicate = referenceColPredicates.get(refColumn);
+                rdfTriples.add(new ImmutableTriple<>(subject, predicate, getReferencedObject(rdbTuples, refColumn)));
+            }
+        }
+
+        return rdfTriples;
     }
 
     private Set<Triple<String, String, String>> getManyToManyTripleSet() throws SQLException {
@@ -53,8 +80,8 @@ public class Table {
         List<String> referenceColumns = this.foreignKeyReferences.keySet().stream().collect(Collectors.toList());
 
         while(rdbTuples.next()) {
-            String subject1 = getManyToManySubject(rdbTuples, referenceColumns.get(0));
-            String subject2 = getManyToManySubject(rdbTuples, referenceColumns.get(1));
+            String subject1 = getReferencedObject(rdbTuples, referenceColumns.get(0));
+            String subject2 = getReferencedObject(rdbTuples, referenceColumns.get(1));
 
             String predicate1 = getManyToManyPredicateString(referenceColumns.get(0));
             String predicate2 = getManyToManyPredicateString(referenceColumns.get(1));
@@ -65,9 +92,29 @@ public class Table {
         return rdfTriples;
     }
 
-    private String getManyToManySubject(ResultSet rdbTuples, String referenceColumn) throws SQLException{
+    private Map<String, String> getValueColPredicates(Set<String> valueColumns) {
+        Map<String, String> vcp = new HashMap<>();
+        for(String vc : valueColumns) {
+            vcp.put(vc, String.format("<%s#%s>", this.tableName, vc));
+        }
+        return vcp;
+    }
+
+    private Map<String, String> getReferenceColPredicates(Set<String> referenceColumns) {
+        Map<String, String> rcp = new HashMap<>();
+        for(String rc : referenceColumns) {
+            rcp.put(rc, String.format("<%s#ref-%s>", this.tableName, rc));
+        }
+        return rcp;
+    }
+
+    private String getReferencedObject(ResultSet rdbTuples, String referenceColumn) throws SQLException{
         String referencedTable = this.foreignKeyReferences.get(referenceColumn);
         return String.format("<%s/%s=%s>", referencedTable, referenceColumn, rdbTuples.getString(referenceColumn));
+    }
+
+    private String getSubject(ResultSet rdbTuples, String pKeyColumn) throws SQLException {
+        return String.format("<%s/%s=%s>", this.tableName, pKeyColumn, rdbTuples.getString(pKeyColumn));
     }
 
     private String getManyToManyPredicateString(String referenceColumn) {
