f2ce97738b14c58389d76241f141f42e21af1869
root <root@localhost.localdomain>
2016-12-02 14:03:07 -0500

    Initial commit

--- src/main/java/com/joeledger/rdb2rdf/App.java
+++ src/main/java/com/joeledger/rdb2rdf/App.java
@@ -1,8 +1,12 @@
 package com.joeledger.rdb2rdf;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.sql.*;
 import org.apache.commons.lang3.tuple.*;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.util.URIref;
 
 public class App 
 {
@@ -11,17 +15,52 @@ public class App
 
         String dbName = "chinook.db";
         Set<Triple<String, String, String>> rdfTriples = new HashSet<>();
+        RdfModel m = new RdfModel();
 
-        try {
-            for(String tableName : DBUtils.getTableNames(dbName)) {
-                Table table = new Table(dbName, tableName);
-                table.fillWithMetadata();
-                rdfTriples.addAll(table.getTripleSet());
+        if(m.createTdb){ //set createTdb true when creating TDB for the first time
+        	try {
+        		for(String tableName : DBUtils.getTableNames(dbName)) {
+        			Table table = new Table(dbName, tableName);
+        			table.fillWithMetadata();
+        			rdfTriples.addAll(table.getTripleSet());
+        		}
+        	} catch (SQLException e) {
+        		System.out.println("Unable to access database");
+        	}
+        	
+        	/*
+        	 * Works specifically for the string triples generated
+        	 * bad coding 
+        	 */
+        	for (Triple<String, String, String> s : rdfTriples) {
+                //System.out.println(s);
+                String subject = s.getLeft();
+                String predicate = s.getMiddle();
+                String object = s.getRight();
+                
+                if(object!=null){
+                	Pattern pattern = Pattern.compile("(.*?)/(.*?)"); 
+                    Matcher mat = pattern.matcher(object);
+                    if(mat.matches()){ //any object with '/' considered resource else literal
+                    	m.model.createResource(subject)
+                		.addProperty(ResourceFactory.createProperty(predicate), 
+                				ResourceFactory.createResource(object));
+                    }
+                    else {
+                    	m.model.createResource(URIref.encode(subject))
+                    	.addProperty(ResourceFactory.createProperty(predicate), 
+                           		object);
+                    }
+                }
+                else {
+                	object="unknown";
+                	m.model.createResource(URIref.encode(subject))
+                	.addProperty(ResourceFactory.createProperty(predicate), 
+                       		object);
+                }
+                        
             }
-        } catch (SQLException e) {
-            System.out.println("Unable to access database");
         }
-
-        System.out.println(rdfTriples);
+        m.query();
     }
 }
